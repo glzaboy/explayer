82a2e98d44b9c8b59dc2ca95f1e7d6a921957d04
guliuzhong <glzaboy@163.com>
2019-03-01 21:28:14 +0800

    升级androidx

---
+++ app/src/main/java/com/qintingfm/explayer/tiny_player/TinyPlayerService.java
@@ -0,0 +1,38 @@
+package com.qintingfm.explayer.tiny_player;
+
+import android.os.Bundle;
+import android.support.v4.media.MediaBrowserCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.media.MediaBrowserServiceCompat;
+
+import java.util.List;
+
+public class TinyPlayerService extends MediaBrowserServiceCompat {
+    private MediaSessionCompat mediaSession;
+    private PlaybackStateCompat.Builder stateBuilder;
+    @Nullable
+    @Override
+    public BrowserRoot onGetRoot(@NonNull String clientPackageName, int clientUid, @Nullable Bundle rootHints) {
+        return null;
+    }
+
+    @Override
+    public void onLoadChildren(@NonNull String parentId, @NonNull Result<List<MediaBrowserCompat.MediaItem>> result) {
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaSession=new MediaSessionCompat(this,this.getClass().getSimpleName());
+
+        mediaSession.setFlags(MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+        setSessionToken(mediaSession.getSessionToken());
+
+    }
+}
